5e0b39f6bbf3cedd50e0e6d6bf60f924f2c44b44
punixcorn <ohyunwoods663@gmail.com>
2024-08-26 19:01:18 +0000

    fixed minor issues

--- bspwm/install.sh
+++ bspwm/install.sh
@@ -1,37 +1,38 @@
 #!/bin/bash
 
+# install files for bspwm
 function log() {
-	printf "\e[32m[*]\e[0m %s\n" "$1"
+    printf "\e[32m[*]\e[0m %s\n" "$1"
 }
 
 function logerr() {
-	printf "\e[33m[-]\e[0m %s\n" "$1"
+    printf "\e[33m[-]\e[0m %s\n" "$1"
 }
 
 function pacman() {
-	log "Pre dependencies setup"
-	git clone https://github.com/punixcorn/arch-setup
-	[ -d arch-setup ] && {
-		chmod +x arch-setup/*
-		./arch-setup/install-choatic-aur.sh
-		./arch-setup/install-yay.sh
-		./arch-setup/install-zsh.sh
-	} || logerr "Cloning failed skipping..."
+    log "Pre dependencies setup"
+    git clone https://github.com/punixcorn/arch-setup
+    [ -d arch-setup ] && {
+        chmod +x arch-setup/*
+        [ -z "$(grep "choatic" /etc/pacman.conf)" ] && echo "choatic available" || ./arch-setup/install-choatic-aur.sh
+        [ ! -f /bin/yay ] && ./arch-setup/install-yay.sh || echo "yay found"
+        [ ! -f /bin/zsh ] && ./arch-setup/install-zsh.sh || echo "zsh found"
+    } || logerr "Cloning failed skipping..."
 
-	sudo pacman -S bspwm sxhkd rofi polybar alacritty dunst feh \
-		xcb-util-cursor xsettingsd mpc mpd dmenu ncmpcpp python-gobject \
-		xfce4-power-manager maim xclip xorg-xbacklight netcat \
-		viewnior python-pywal xdg-user-dirs firefox chromium xorg-xrandr python --noconfirm
+    sudo pacman -S bspwm sxhkd rofi polybar alacritty dunst feh \
+        xcb-util-cursor xsettingsd mpc mpd dmenu ncmpcpp python-gobject \
+        xfce4-power-manager maim xclip xorg-xbacklight netcat \
+        viewnior python-pywal xdg-user-dirs firefox chromium xorg-xrandr python --noconfirm
 }
 
 function debain() {
-	sudo apt update
-	sudo apt install bspwm sxhkd rofi polybar alacritty dunst feh \
-		xsettingsd mpc mpd dmenu ncmpcpp \
-		xfce4-power-manager maim xclip xorg-xbacklight netcat \
-		viewnior xdg-user-dirs firefox chromium xorg-xrandr python3 -y
+    sudo apt update
+    sudo apt install bspwm sxhkd rofi polybar alacritty dunst feh \
+        xsettingsd mpc mpd dmenu ncmpcpp alsa-utils network-manager network-manager-dev\
+        xfce4-power-manager maim xclip light netcat xss-lock picom \
+        viewnior xdg-user-dirs firefox chromium xorg-xrandr python3 -y
 
-	python -m pip install --upgrade gobject pywal
+    python -m pip install --upgrade gobject pywal
 }
 
 log "Installing Dependencies"
@@ -40,16 +41,16 @@ log "Installing Dependencies"
 log "Copying files over"
 CONFDIR="$HOME/.config/bspwm/"
 
-[ -d $CONFDIR ] && mv $CONFDIR $HOME/.config/bspwm-backup
+[ -d $CONFDIR ] && mv $CONFDIR $HOME/.config/bspwm-backup-$(date "+%a-%T")
 [ ! -d $CONFDIR ] && mkdir -p $CONFDIR
 cp -r ** $CONFDIR
 
 [ -d $CONFDIR/others ] && {
-	[ ! -d $HOME/.config/networkmanager-dmenu/ ] && mkdir -p $HOME/.config/networkmanager-dmenu
-	cp -r $CONFDIR/others/config.ini $HOME/.config/networkmanager-dmenu
-	[ ! -d $HOME/.local/share/fonts ] && mkdir -p $HOME/.local/share/fonts
-	sudo cp -r $CONFDIR/others/fonts/* $HOME/.local/share/fonts
-	fc-cache -fv
+    [ ! -d $HOME/.config/networkmanager-dmenu/ ] && mkdir -p $HOME/.config/networkmanager-dmenu
+    cp -r $CONFDIR/others/config.ini $HOME/.config/networkmanager-dmenu
+    [ ! -d $HOME/.local/share/fonts ] && mkdir -p $HOME/.local/share/fonts
+    sudo cp -r $CONFDIR/others/fonts/* $HOME/.local/share/fonts
+    fc-cache -fv
 } || logerr "Minor Dependencies not found (others folder not found)"
 
 [ -f /bin/wal ] && wal -i $CONFDIR/wallpaper.png || logerr "wal failed, Mannual intervention needed"
@@ -59,6 +60,38 @@ log "Creating Xinitrc"
 cat <<EOF >$HOME/.xinitrc
 exec bspwm
 EOF
-# sudo usermod -aG video $USER
+
+log "fixing light"
+sudo usermod -aG video $USER
+
+log "Laptop touchpad fix"
+xorg_touch_file="50-libinput.conf"
+[ -f /etc/X11/xorg.conf.d/${xorg_touch_file} ] && xorg_touch_file="231-libinput.conf"
+sudo cat <<EOF >/etc/X11/xorg.conf.d/$xorg_touch_file
+Section "InputClass"
+        Identifier "libinput touchpad catchall"
+        MatchIsTouchpad "on"
+        MatchDevicePath "/dev/input/event*"
+        Driver "libinput"
+        Option "Tapping" "on"
+EndSection
+EOF
+
+if [ $ans = 'Y' ] || [ $ans = 'y' ];then 
+    [ -f /bin/pacman ] && sudo pacman -S neovim vim || sudo apt install neovim vim 
+    [ -d ~/.vim ] && mv ~/.vim ~/.vim-$(date "+%a-%T")
+    [ -d ~/.config/nvim ] && mv ~/.config/nvim ~/.config/nvim-$(date "+%a-%T")
+
+    log "Installing vim"
+    git clone https://github.com/punixcorn/vim-dots ~/.vim 
+    chmod +x ~/.vim/install.sh
+    bash ~/.vim/install.sh
+
+
+    log "Installing neovim"
+    git clone https://github.com/punixcorn/nvim-dots ~/.config/nvim
+    chmod +x ~/.config/nvim 
+    bash ~/.config/nvim/install.sh
+fi
 
 exit 0
--- i3/install.sh
+++ i3/install.sh
@@ -1,27 +1,44 @@
 #!/bin/bash
 
+# install files for i3 
+
 function log() {
-	printf "\e[32m[*]\e[0m %s\n" "$1"
+    printf "\e[32m[*]\e[0m %s\n" "$1"
 }
 
 function logerr() {
-	printf "\e[33m[-]\e[0m %s\n" "$1"
+    printf "\e[33m[-]\e[0m %s\n" "$1"
 }
 
-log "Pre dependencies setup"
-git clone https://github.com/punixcorn/arch-setup
-[ -d arch-setup ] && {
-	chmod +x arch-setup/*
-	./arch-setup/install-choatic-aur.sh
-	./arch-setup/install-yay.sh
-	./arch-setup/install-zsh.sh
-} || logerr "Cloning failed skipping..."
+function pacman() {
+    log "Pre dependencies setup"
+    git clone https://github.com/punixcorn/arch-setup
+    [ -d arch-setup ] && {
+        chmod +x arch-setup/*
+        [ -z "$(grep "choatic" /etc/pacman.conf)" ] && echo "choatic available" || ./arch-setup/install-choatic-aur.sh
+        [ ! -f /bin/yay ] && ./arch-setup/install-yay.sh || echo "yay found"
+        [ ! -f /bin/zsh ] && ./arch-setup/install-zsh.sh || echo "zsh found"
+    } || logerr "Cloning failed skipping..."
+
+    sudo pacman -Sy i3-wm picom dunst polybar xss-lock xfce4-power-manager mpd feh i3lock rofi alacritty conky uthash meson base-devel cmake ninja python-pywal picom feh -no-confirm
+
+    [ -f /bin/yay ] && yay -S light xfce-polkit || logerr "Failed to install from yay"
+    sudo pacman -S networkmanager-dmenu-git || logerr "Failed to install networkmanager-dmenu-git, please do it mannually"
+
+}
+
+function debain() {
+    sudo apt update
+    sudo apt install i3 rofi polybar alacritty dunst feh xss-lock \
+        xsettingsd mpc mpd dmenu ncmpcpp alsa-utils network-manager network-manager-dev  \
+        xfce4-power-manager maim xclip light netcat picom \
+        viewnior xdg-user-dirs firefox chromium xorg-xrandr python3 -y
+
+    python -m pip install --upgrade gobject pywal
+}
 
 log "Installing Dependencies"
-sudo pacman -Sy i3-wm picom dunst polybar xss-lock xfce4-power-manager mpd feh i3lock rofi alacritty conky uthash meson base-devel cmake ninja python-pywal picom feh
-
-[ -f /bin/yay ] && yay -S light xfce-polkit || logerr "Failed to install from yay"
-sudo pacman -S networkmanager-dmenu-git || logerr "Failed to install networkmanager-dmenu-git, please do it mannually"
+[ -f /bin/pacman ] && pacman || debain
 
 # making picom always fails do mannually
 # cd /tmp
@@ -34,10 +51,10 @@ sudo pacman -S networkmanager-dmenu-git || logerr "Failed to install networkmana
 
 log "Copying files over"
 [ ! -d "$HOME/.config/i3/" ] && mkdir -p $HOME/.config/i3 || {
-	echo "i3 config found"
-	mv $HOME/.config/i3/ $HOME/.config/i3-backup
-	echo "backup done"
-	mkdir -p "$HOME/.config/i3/"
+    echo "i3 config found"
+    mv $HOME/.config/i3/ $HOME/.config/i3-backup-$(date "+%a-%T")
+    echo "backup done"
+    mkdir -p "$HOME/.config/i3/"
 
 }
 cp -r * "$HOME/.config/i3/"
@@ -53,3 +70,40 @@ log "Creating Xinitrc"
 cat <<EOF >$HOME/.xinitrc
 exec i3
 EOF
+
+log "fixing light"
+sudo usermod -aG video $USER
+
+log "Laptop touchpad fix"
+xorg_touch_file="50-libinput.conf"
+[ -f /etc/X11/xorg.conf.d/${xorg_touch_file} ] && xorg_touch_file="231-libinput.conf"
+sudo cat <<EOF >/etc/X11/xorg.conf.d/$xorg_touch_file
+Section "InputClass"
+        Identifier "libinput touchpad catchall"
+        MatchIsTouchpad "on"
+        MatchDevicePath "/dev/input/event*"
+        Driver "libinput"
+        Option "Tapping" "on"
+EndSection
+EOF
+
+echo "install nvim and vim configs? [y,N]"
+read ans
+
+if [ $ans = 'Y' ] || [ $ans = 'y' ];then 
+    [ -f /bin/pacman ] && sudo pacman -S neovim vim || sudo apt install neovim vim 
+    [ -d ~/.vim ] && mv ~/.vim ~/.vim-$(date "+%a-%T")
+    [ -d ~/.config/nvim ] && mv ~/.config/nvim ~/.config/nvim-$(date "+%a-%T")
+
+    log "Installing vim"
+    git clone https://github.com/punixcorn/vim-dots ~/.vim 
+    chmod +x ~/.vim/install.sh
+    bash ~/.vim/install.sh
+
+    log "Installing neovim"
+    git clone https://github.com/punixcorn/nvim-dots ~/.config/nvim
+    chmod +x ~/.config/nvim 
+    bash ~/.config/nvim/install.sh
+fi
+
+exit 0
